0d855955649a7e3f14487bb5f7fb0e8917264636
João Parada <up201405280@edu.fe.up.pt>
2023-10-26 12:13:56 +0100

    Mockito test implemented

--- src/test/java/com/aor/numbers/ListAggregatorTest.java
+++ src/test/java/com/aor/numbers/ListAggregatorTest.java
@@ -3,6 +3,7 @@ package com.aor.numbers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.util.Arrays;
 import java.util.List;
@@ -50,18 +51,15 @@ public class ListAggregatorTest {
 
     @Test
     public void distinct_bug_8726(){
-        class DistinctStub implements GenericListDeduplicator{
+        ListSorter genericlistsorter = new ListSorter();
 
-            @Override
-            public List<Integer> deduplicate(List<Integer> list, GenericSorter genericsorter) {
-                return Arrays.asList(1,2,4);
-            }
-
-        }
-        GenericListDeduplicator listdeduplicator = new DistinctStub();
+        GenericListDeduplicator deduplicator = Mockito.mock(GenericListDeduplicator.class);
         List<Integer> list2 = Arrays.asList(1,2,4,2);
+        Mockito.when(deduplicator.deduplicate(list2 ,genericlistsorter)).thenReturn(Arrays.asList(1, 2, 4));
+
+
         ListAggregator aggregator = new ListAggregator();
-        int distinct = aggregator.distinct(list2, listdeduplicator, null );
+        int distinct = aggregator.distinct(list2, deduplicator, genericlistsorter);
 
         Assertions.assertEquals(3, distinct);
     }
